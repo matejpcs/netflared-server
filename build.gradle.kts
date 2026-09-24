d11bc2e7f02fb0bf8b3864c8a601fa529b3ed981
MatějPCs <workspacematej@gmail.com>
2026-09-24 18:20:41 +0200

    Fix Gradle repository configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,6 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 }
 
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-}
-
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2.build.129-stable")
     compileOnly("com.google.code.gson:gson:2.13.2")
